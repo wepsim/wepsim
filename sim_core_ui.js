f4aa2f3c0d0c4bab9c1e8923ea24842e44ca7e95
Alejandro Calderon <acaldero@arcos.inf.uc3m.es>
2015-12-04 23:03:51 +0100

    ui improvement: single IR

--- sim_core_ui.js
+++ sim_core_ui.js
@@ -197,7 +197,7 @@
                               "REG_PC,0",        "REG_MAR,0", "REG_MBR,0",    "REG_IR,0", 
                               "REG_RT1,0",       "REG_RT2,0", "REG_RT3,0",    "REG_SR,0", 
                               "FLAG_O,0",        "FLAG_N,0",  "FLAG_Z,0",     "FLAG_I,0",    "FLAG_U,0", 
-                              "REG_MICROADDR,0", "REG_IR,0" ] ;
+                              "REG_MICROADDR,0" ] ;
 
         var divclasses = [ "col-xs-3 col-sm-3 col-md-3 col-lg-2",
                            "col-xs-6 col-sm-6 col-md-6 col-lg-6" ] ;
