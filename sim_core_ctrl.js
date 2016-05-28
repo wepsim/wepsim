1681aca0a92dd60b730c3faad06ab14d71af4796
Alejandro <acaldero.uc3m@gmail.com>
2016-05-28 09:14:48 +0200

    Initial en-es Help support

--- sim_core_ctrl.js
+++ sim_core_ctrl.js
@@ -228,7 +228,8 @@
         function update_signal_loadhelp ( helpdiv, key )
         {
 	     $(helpdiv).collapse('toggle');
-	     $(helpdiv).load('signals-' + ws_idiom + '.html #' + key, 
+	     var help_base = 'help/signals-' + ws_idiom + '.html #' + key;
+	     $(helpdiv).load(help_base,
 			      function(response, status, xhr) { 
 				  if ( $(helpdiv).html() == "" ) 
 				       $(helpdiv).html('<br>Sorry, No more details available for this signal.<p>\n'); 
