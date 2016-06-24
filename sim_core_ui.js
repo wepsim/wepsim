a0e6f394086a2e830aef34f6dfc6f5b8363e3d12
Alejandro <acaldero.uc3m@gmail.com>
2016-06-24 23:00:37 +0200

    minor visual improvements...

--- sim_core_ui.js
+++ sim_core_ui.js
@@ -200,9 +200,9 @@
 
 	    for (var index=0; index < sim_states['BR'].length; index++) 
             {
-		 o1_rf += "<div class='col-xs-2 col-sm-1 col-md-1 col-lg-1' id='name_RF" + index + "' style='padding: 0 15 0 5;'>" +
+		 o1_rf += "<div class='col-xs-2 col-sm-1 col-md-2 col-lg-1' id='name_RF" + index + "' style='padding: 0 15 0 5;'>" +
                           "R" + index + "</div>" + 
-                          "<div class='col-xs-4 col-sm-2 col-md-3 col-lg-3' id='tbl_RF"  + index + "' style='padding: 0 5 0 35;'>" +
+                          "<div class='col-xs-4 col-sm-2 col-md-4 col-lg-3' id='tbl_RF"  + index + "' style='padding: 0 5 0 35;'>" +
                           (sim_states['BR'][index] >>> 0).toString(RF_display_format).toUpperCase() + "</div>" ; 
 	    }
 
