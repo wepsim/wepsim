ba7540be3b4dbaaa83cb5957c473f8adfdb4af6d
Alejandro <acaldero.uc3m@gmail.com>
2016-07-30 10:12:57 +0200

    three level examples...

--- sim_cfg.js
+++ sim_cfg.js
@@ -20,7 +20,7 @@
 
 
         var WSCFG = new Object() ;
-        WSCFG['version'] = { value:"1.5.8", type:"string"} ;
+        WSCFG['version'] = { value:"1.5.9", type:"string"} ;
 
         function get_cfg ( field )
         {
