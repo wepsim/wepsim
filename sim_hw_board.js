45d6c375a737a5c5afbea9ea9c67f99f08bf6c78
Alejandro Calderon <acaldero.uc3m@gmail.com>
2016-08-03 15:07:58 +0200

    General updates...

--- sim_hw_board.js
+++ sim_hw_board.js
@@ -1,5 +1,5 @@
 /*      
- *  Copyright 2015-2016 Javier Prieto Cepeda, Felix Garcia Carballeira, Alejandro Calderon Mateos
+ *  Copyright 2015-2017 Felix Garcia Carballeira, Alejandro Calderon Mateos, Javier Prieto Cepeda, Saul Alonso Monsalve
  *
  *  This file is part of WepSIM.
  * 
--- wepsim.js
+++ wepsim.js
@@ -1,5 +1,5 @@
 /*      
- *  Copyright 2015-2016 Alejandro Calderon Mateos, Felix Garcia Carballeira, Saul Alonso Monsalve, Javier Prieto Cepeda
+ *  Copyright 2015-2017 Felix Garcia Carballeira, Alejandro Calderon Mateos, Javier Prieto Cepeda, Saul Alonso Monsalve
  *
  *  This file is part of WepSIM.
  * 
