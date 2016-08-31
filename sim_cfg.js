b791f63785a62a5783b5a89eb7d43adfa81277bc
Alejandro Calderon <acaldero@inf.uc3m.es>
2016-08-31 20:12:52 +0200

    minor update

--- sim_cfg.js
+++ sim_cfg.js
@@ -20,7 +20,7 @@
 
 
         var WSCFG = new Object() ;
-        WSCFG['version'] = { value:"1.6.2", type:"string"} ;
+        WSCFG['version'] = { value:"1.6.3", type:"string"} ;
 
         function get_cfg ( field )
         {
