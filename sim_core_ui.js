e75f9fbd88c361fda8fcfb247594dd97ca708975
Alejandro <acaldero.uc3m@gmail.com>
2016-12-02 12:18:03 +0100

    minor update

--- sim_core_ui.js
+++ sim_core_ui.js
@@ -1014,7 +1014,7 @@
 				"M1,0",   "M2,0",   "M7,0",  "MA,0",   "MB,0",  
                                 "SELP,0", "LC,0",   "SE,0",  "SIZE,0", "OFFSET,0",
                                 "BW,0",   "R,0",    "W,0",   "TA,0",   "TD,0",   "IOR,0",  "IOW,0",  
-                                "I,0",    "U,0"  ] ;
+                                "TEST_I,0",    "TEST_U,0"  ] ;
 
 		var h = "<tr bgcolor=#FF9900>" + 
                         "<td bgcolor=white     style='border-style: solid; border-width:0px; border-color:lightgray;'></td>" + 
