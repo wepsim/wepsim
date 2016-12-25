48234766d907792eb022a517f3ed49364b11101f
Alejandro <acaldero.uc3m@gmail.com>
2016-12-25 11:53:39 +0100

    sync with wepsim

--- examples/examples.js
+++ examples/examples.js
@@ -73,9 +73,3 @@
                      description: "Advanced example with interruption, system call, and exception.<br>" 
                   });
 
-    examples.push({ 
-                     id: 'S4E1',
-                     title: "Special: addv + seqv.",
-                     description: "Example of instructions addv and seqv.<br>" 
-                  });
-
