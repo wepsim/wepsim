ebc5a84c7569899ee0a62f4e6e7132eb9658add1
Alejandro <acaldero.uc3m@gmail.com>
2017-01-08 11:53:14 +0100

    help update

--- help/helps.js
+++ help/helps.js
@@ -4,42 +4,44 @@
     helps.push({
                      id:          "simulator",
                      title:       "Simulator usage",
+                     type:        "relative",
                      reference:   "#help_simulator_screens",
-                     description: "Simulator usage.<br>"
+                     description: "Description of how the simulator works.<br>"
                   });
 
     helps.push({
                      id:          "microcode",
                      title:       "Microcode format",
+                     type:        "relative",
                      reference:   "#help_firmware_format",
-                     description: "Simulator usage.<br>"
+                     description: "Syntax of the microcode used.<br>"
                   });
 
     helps.push({
                      id:          "assembly",
                      title:       "Assembly format",
+                     type:        "relative",
                      reference:   "#help_assembly_format",
-                     description: "Assembly format.<br>"
+                     description: "Syntax of the assembly elements.<br>"
                   });
 
     helps.push({
-                     id:          "simulator",
+                     id:          "architecture",
                      title:       "Simulator architecture",
+                     type:        "relative",
                      reference:   "#help_simulator_arch",
-                     description: "Simulator architecture.<br>"
+                     description: "Description of the simulator architecture.<br>"
                   });
 
-/*
-    helps.push({ 
-                     id: 'signals',
-                     title: "Signals",
-                     description: "Signals.<br>"
+    helps.push({
+                     id:          "architecture",
+                     title:       "Hardware summary",
+                     type:        "code",
+                     reference:   "$(\'#help1_ref\').data(\'relative\',\'\');" +
+                                  "$(\'#iframe_help1\').html(\'<object id=svg_p2 data=\\'images/cpu6.svg?time=20170108\\' type=image/svg+xml>Your browser does not support SVG</object>\');" +
+                                  "$(\'#help1\').trigger(\'updatelayout\');" +
+                                  "$(\'#help1\').popup(\'reposition\', {positionTo: \'window\'});" +
+                                  "$(\'#help1\').popup(\'open\');",
+                     description: "Reference card of the elemental processor hardware simulated.<br>"
                   });
-
-    helps.push({ 
-                     id: 'simulator',
-                     title: "Simulator",
-                     description: "Simulator.<br>"
-                  });
-*/
 
