443d3c8c6f4056008b0a05386719f2e554301a5c
Alejandro <acaldero.uc3m@gmail.com>
2017-01-12 22:59:18 +0100

    minor update

--- help/helps.js
+++ help/helps.js
@@ -60,7 +60,7 @@
 
     helps.push({
                      id:          "about",
-                     title:       "License, platforms , technologies",
+                     title:       "License, platforms, technologies",
                      type:        "absolute",
                      reference:   "about",
                      description: "WepSIM license, supported platforms, technologies used.<br>"
