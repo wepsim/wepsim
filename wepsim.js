2a841c9ad7efa0d2b12a0e95167453633f67493f
Alejandro <acaldero.uc3m@gmail.com>
2017-01-17 22:15:53 +0100

    tutorial radio updated

--- wepsim.js
+++ wepsim.js
@@ -830,6 +830,7 @@
 		    callback: function() {
 			set_cfg('show_tutorials', false) ;
                         save_cfg();
+                        $("#radio10-true").prop('checked', false).checkboxradio("refresh") ;
                         $("#radio10-false").prop('checked', true).checkboxradio("refresh") ;
                         tutbox.modal("hide") ;
 		    }
