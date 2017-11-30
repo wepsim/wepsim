420a5d8f6778922a425fbaa20ad1a71e771748e8
Alejandro <acaldero.uc3m@gmail.com>
2017-11-30 12:43:48 +0100

    minor improvements

--- sim_cfg.js
+++ sim_cfg.js
@@ -20,7 +20,7 @@
 
 
         var WSCFG = new Object() ;
-        WSCFG['version'] = { value:"1.8.3", type:"string"} ;
+        WSCFG['version'] = { value:"1.8.5", type:"string"} ;
 
         function get_cfg ( field )
         {
