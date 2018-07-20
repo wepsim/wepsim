aaddeba4c8f902ccfef46bc2ace207098ee3d644
Alejandro <acaldero@inf.uc3m.es>
2018-07-20 21:15:11 +0200

    help improved

--- wepsim/es/help-es.js
+++ wepsim/es/help-es.js
@@ -23,36 +23,54 @@
 
     help.es.push({
                      id:          "simulator",
-                     title:       "Uso del simulador",
+                     title:       "Simulador: microc&oacute;digo",
                      i_type:      "relative",
-                     u_type:      "manual",
-                     reference:   "#help_simulator_screens",
-                     description: "Descripción de cómo funciona el simulador.<br>"
+                     u_type:      "simulador",
+                     reference:   "#help_simulator_firmware",
+                     description: "Descripción de cómo trabajar en el simulador con el microc&oacute;digo.<br>"
                   });
 
     help.es.push({
                      id:          "microcode",
                      title:       "Formato del microcódigo",
                      i_type:      "relative",
-                     u_type:      "manual",
+                     u_type:      "simulador",
                      reference:   "#help_firmware_format",
                      description: "Sintáxis del microcódigo usado.<br>"
+                  });
+
+    help.es.push({
+                     id:          "simulator",
+                     title:       "Simulador: ensamblador",
+                     i_type:      "relative",
+                     u_type:      "simulador",
+                     reference:   "#help_simulator_assembly",
+                     description: "Descripción de cómo trabajar en el simulador con el ensamblador.<br>"
                   });
 
     help.es.push({
                      id:          "assembly",
                      title:       "Formato del ensamblador",
                      i_type:      "relative",
-                     u_type:      "manual",
+                     u_type:      "simulador",
                      reference:   "#help_assembly_format",
                      description: "Sintáxis del ensamblador.<br>"
+                  });
+
+    help.es.push({
+                     id:          "simulator",
+                     title:       "Simulador: ejecuci&oacute;n",
+                     i_type:      "relative",
+                     u_type:      "simulador",
+                     reference:   "#help_simulator_execution",
+                     description: "Descripción de cómo ejecutar en el simulador el ensamblador y microc&oacute;digo.<br>"
                   });
 
     help.es.push({
                      id:          "architecture",
                      title:       "Arquitectura del simulador",
                      i_type:      "relative",
-                     u_type:      "manual",
+                     u_type:      "procesador elemental",
                      reference:   "#help_simulator_arch",
                      description: "Descripción de la arquitectura del procesador elemental.<br>"
                   });
@@ -61,7 +79,7 @@
                      id:          "architecture",
                      title:       "Señales simuladas",
                      i_type:      "absolute",
-                     u_type:      "manual",
+                     u_type:      "procesador elemental",
                      reference:   "signals",
                      description: "Resumen de las señales principales del procesador elemental.<br>"
                   });
@@ -70,7 +88,7 @@
                      id:          "architecture",
                      title:       "Resumen del Hardware",
                      i_type:      "code",
-                     u_type:      "manual",
+                     u_type:      "procesador elemental",
                      reference:   "wepsim_open_help_content(\'<object id=svg_p2 data=\\'sim_hw/sim_hw_ep/cpu6.svg?time=20180108\\' type=image/svg+xml>Your browser does not support SVG</object>\');",
                      description: "Resumen del hardware del procesador elemental simulado.<br>"
                   });
