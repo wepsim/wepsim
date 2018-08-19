1f826b5b1f91bf4936860ccb2e7913b6f6b1d3b7
Alejandro Calderon Mateos (UC3M) <acaldero@inf.uc3m.es>
2018-08-19 22:41:56 +0200

    1.9.2

--- sim_core/sim_core_ui_help.js
+++ sim_core/sim_core_ui_help.js
@@ -39,16 +39,21 @@
 
      function update_div_frompartialhtml ( helpdiv, key, data )
      {
-	var content = '<br>Sorry, No more details available for this element.<p>\n' ;
-	if (data != "") 
+	var default_content = '<br>Sorry, No more details available for this element.<p>\n' ;
+	var help_content    = default_content ;
+
+	if ("" != data) 
 	{
-	    content = $(data).find('#' + key).html() ;
-	    if (typeof content == "undefined") {
-		content = $(data).filter('#' + key).html() ;
-	    }
+		help_content = $(data).find('#' + key).html() ;
+		if (typeof help_content == "undefined") {
+		    help_content = $(data).filter('#' + key).html() ;
+		}
+		if (typeof help_content == "undefined") {
+		    help_content = default_content ;
+		}
 	}
 
-	$(helpdiv).html(content) ;
+	$(helpdiv).html(help_content) ;
 	$(helpdiv).trigger('create');
      }
 
