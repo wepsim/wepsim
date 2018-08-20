20d4c4b0c9809e3f8c910a4251e847eddbfcce31
Alejandro Calderon Mateos (UC3M) <acaldero@inf.uc3m.es>
2018-08-20 12:35:30 +0200

    1.9.2

--- wepsim/wepsim_misc.js
+++ wepsim/wepsim_misc.js
@@ -455,32 +455,32 @@
     function wepsim_show_asm_columns_checked ( asm_div )
     {
         var o = '<ul class="list-group">' +
-                '<li class="list-group-item py-0"><label>' + 
+                '<li class="list-group-item py-0"><label class="m-1">' + 
 		'<input aria-label="Show breakpoint" type="checkbox" value="1" id="asm_break" ' + 
                 '    checked="checked" data-toggle="collapse" data-target=".asm_break">' + 
                 '&nbsp;<span>breakpoints</span>' + 
 		'</label></li>' +
-		'<li class="list-group-item py-0"><label>' + 
+		'<li class="list-group-item py-0"><label class="m-1">' + 
 		'<input aria-label="Show address"    type="checkbox" value="1" id="asm_addr"    ' + 
 		'    checked="checked" data-toggle="collapse" data-target=".asm_addr">' + 
                 '&nbsp;<span>address</span>' + 
 		'</label></li>' +
-		'<li class="list-group-item py-0"><label>' + 
+		'<li class="list-group-item py-0"><label class="m-1">' + 
 		'<input aria-label="Show label"      type="checkbox" value="1" id="asm_label2"  ' + 
 		'    checked="checked" data-toggle="collapse" data-target=".asm_label2">' + 
                 '&nbsp;<span>labels</span>' + 
 		'</label></li>' +
-                '<li class="list-group-item py-0"><label>' + 
+                '<li class="list-group-item py-0"><label class="m-1">' + 
 		'<input aria-label="Show pseudo-instruction" type="checkbox" value="1" id="asm_pins" ' + 
 		'    checked="checked" data-toggle="collapse" data-target=".asm_pins">' + 
                 '&nbsp;<span>pseudo</span><span class="d-none d-md-inline">-instructions</span>' + 
 		'</label></li>' +
-		'<li class="list-group-item py-0"><label>' + 
+		'<li class="list-group-item py-0"><label class="m-1">' + 
 		'<input aria-label="Show label"              type="checkbox" value="1" id="asm_label1"  ' + 
 		' checked="checked" data-toggle="collapse" data-target=".asm_label1">' + 
                 '&nbsp;<span>labels</span>' + 
 		'</label></li>' +
-		'<li class="list-group-item py-0"><label>' + 
+		'<li class="list-group-item py-0"><label class="m-1">' + 
 		'<input aria-label="Show instruction"        type="checkbox" value="1" id="asm_ins"     ' + 
 		' checked="checked" data-toggle="collapse" data-target=".asm_ins">' + 
                 '&nbsp;<span>assembly</span>' + 
