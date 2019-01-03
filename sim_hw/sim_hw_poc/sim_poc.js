1c72063b2e32ed06868bd645b144abb0290e5ff6
Alejandro Calderon Mateos <acaldero.uc3m@gmail.com>
2019-01-03 01:29:10 +0100

    1.9.6

--- sim_hw/sim_hw_poc/sim_poc.js
+++ sim_hw/sim_hw_poc/sim_poc.js
@@ -40,9 +40,9 @@
         var poc_def = {
                        sim_name:            "Proof-Of-Concept Processor",
                        sim_short_name:      "poc",
-                       sim_img_processor:   "examples/hardware/poc/images/processor3.svg",
-                       sim_img_controlunit: "examples/hardware/poc/images/controlunit2.svg",
-                       sim_img_cpu:         "examples/hardware/poc/images/cpu2.svg",
+                       sim_img_processor:   "examples/hardware/poc/images/processor6.svg",
+                       sim_img_controlunit: "examples/hardware/poc/images/controlunit6.svg",
+                       sim_img_cpu:         "examples/hardware/poc/images/cpu6.svg",
 
                        components:          poc_components,
                        states:              poc_states,
