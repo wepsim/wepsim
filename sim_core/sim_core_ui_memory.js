ba2e3d4cc1f924edbdcbbd9b090c88000b9258fb
Alejandro Calderon Mateos <acaldero.uc3m@gmail.com>
2019-01-07 19:02:48 +0100

    1.9.6

--- sim_core/sim_core_ui_memory.js
+++ sim_core/sim_core_ui_memory.js
@@ -667,17 +667,17 @@
                      o +=  "<tr id='asmdbg" + l + "' bgcolor='" + asm[l].bgcolor + "'" +
                            "    onclick='asmdbg_set_breakpoint(" + l + "); " +
                            "             if (event.stopPropagation) event.stopPropagation();'>" +
-                           "<td class='asm_label  text-monospace collapse show col-auto' " +
+                           "<td class='asm_label  text-monospace col-auto collapse show' " +
                            "    style='line-height:0.9;' align=right>" + s_label + "</td>" +
-                           "<td class='asm_addr   text-monospace collapse show col-auto' " +
+                           "<td class='asm_addr   text-monospace col-auto collapse show' " +
                            "    style='line-height:0.9;'>" + l + "</td>" +
-                           "<td class='asm_break  text-monospace collapse show col-1 py-0 px-2' " +
-                           "    style='line-height:0.9;' id='bp" + l + "'>" + "</td>" +
-                           "<td class='asm_hex    text-monospace collapse      col-auto' " +
+                           "<td class='asm_break  text-monospace col-auto collapse show py-0 px-2' " +
+                           "    style='line-height:0.9;' id='bp" + l + "' width='1%'>" + "</td>" +
+                           "<td class='asm_hex    text-monospace col-auto collapse     ' " +
                            "    style='line-height:0.9;'>" + s3_hex + "</td>" +
-                           "<td class='asm_ins    text-monospace collapse show col-auto' " +
+                           "<td class='asm_ins    text-monospace col-auto collapse show' " +
                            "    style='line-height:0.9;' align=left>" + s1_instr + "</td>" +
-                           "<td class='asm_pins   text-monospace collapse show col-auto' " +
+                           "<td class='asm_pins   text-monospace col-auto collapse show' " +
                            "    style='line-height:0.9;' align=left>" + s2_instr + "</td>" +
                            "</tr>" ;
                 }
