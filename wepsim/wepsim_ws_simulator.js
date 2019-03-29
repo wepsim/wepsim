7afc0844c26a413086893e8ae14c350a13f21642
Alejandro <acaldero@inf.uc3m.es>
2019-03-29 18:43:39 +0100

    2.0.3

--- wepsim/wepsim_ws_simulator.js
+++ wepsim/wepsim_ws_simulator.js
@@ -401,40 +401,38 @@
     }
 
 
-    // preload
+    /*
+     * Preload work
+     */
 
-    function wepsim_preload ( parameters )
+    function wepsim_preload_hash ( hash )
     {
 	    var o = '' ;
 
 	    // parameter: mode
-	    var mode_reference = parameters.get('mode') ;
-	    if (mode_reference !== null)
+	    if (hash['mode'] !== null)
 	    {
-                simui_select_main(mode_reference) ;
-	        o += '<li>Mode set to ' + mode_reference + '.</li> ' ;
+                simui_select_main(hash['mode']) ;
+	        o += '<li>Mode set to <strong>' + hash['mode'] + '</strong>.</li> ' ;
 	    }
 
 	    // parameter: example
-	    var example_reference = parameters.get('example') ;
-	    if (example_reference !== null)
+	    if (hash['example'] !== null)
 	    {
-		var example_index = parseInt(example_reference) ;
+		var example_index = parseInt(hash['example']) ;
 		var example_obj   = examples.en[example_index] ;
 	        if (typeof example_obj !== "undefined")
 		{
-		    var example_uri   = example_obj.hardware + ":" + example_obj.microcode + ":" + example_obj.assembly ;
+		    var example_uri = example_obj.hardware + ":" + example_obj.microcode + ":" + example_obj.assembly ;
 	            load_from_example_firmware(example_uri, true) ;
-	            o += '<li>Example ' + example_index + ' loaded.</li> ' ;
+	            o += '<li>Example <strong>' + example_index + '</strong> loaded.</li> ' ;
 		}
 	    }
 
 	    // parameter: simulator UI
-	    var panels_reference = parameters.get('simulator') ;
-
 	    var panels = [] ;
-	    if (panels_reference !== null) {
-	        panels = panels_reference.split(":") ;
+	    if (hash['simulator'] !== null) {
+	        panels = hash['simulator'].split(":") ;
 	        o += '<li>User interface adapted to the example.</li> ' ;
             }
 
@@ -459,14 +457,59 @@
 
 	    // notify the user of the preloaded work
 	    if (o !== '') {
-		o = 'WepSIM has been instructed to preload some work for you:<br><ul>' + 
-		    o + 
-		    '</ul>' + 
-	            'Now you can, for example, press the <strong>Run</strong> button to execute the example and see the results.' ;
+		o = 'WepSIM has been instructed to preload some work for you:<br>' + 
+		    '<ul>' + o + '</ul>' + 
+		    'To close this notification please press in the "X". <br>' +
+	            'In order to execute an example please press the ' + 
+		    '<span class="btn btn-sm btn-info py-0" onclick="wepsim_execute_toggle_play(\'#qbp\',false);">Run</span> ' + 'button. <br>' ;
+
 	        simcoreui_notify('WepSIM preloads some work', o, 'info', 0) ;
 	    }
 
 	    // return ok
 	    return 0 ;
+    }
+
+    function wepsim_preload_json ( json_url )
+    {
+	    var size = 0 ;
+	    var max_size = 8*1024 ;
+
+	    // preload json_url only if file_size(json_url) < 8 KB
+	    var xhr = new XMLHttpRequest() ;
+	    xhr.open("HEAD", json_url, true) ;
+
+	    xhr.onreadystatechange = function() {
+		if (this.readyState == this.DONE) {
+		    size = parseInt(xhr.getResponseHeader("Content-Length")) ;
+		    if (size < max_size) {
+	                $.getJSON(json_url, wepsim_preload_hash) ;
+		    }
+		}
+	    } ;
+
+	    xhr.send();
+    }
+
+    function wepsim_preload_get ( parameters )
+    {
+	    var hash = {} ;
+
+	    // 1.- get parameters
+	    hash['mode']      = parameters.get('mode') ;
+	    hash['example']   = parameters.get('example') ;
+	    hash['simulator'] = parameters.get('simulator') ;
+	    hash['preload']   = parameters.get('preload') ;
+
+	    // 2.a.- json
+	    if (hash['preload'] !== null) 
+	    {
+	        var uri_obj = new URL(hash['preload']) ;
+	        wepsim_preload_json(uri_obj.pathname) ;
+		return ;
+	    }
+
+	    // 2.b.- hash
+	    wepsim_preload_hash(hash) ;
     }
 
