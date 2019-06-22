b1aef86b0af91ad4d8487ac5fbae254aa697489b
Alejandro <acaldero@inf.uc3m.es>
2019-06-22 12:26:51 +0200

    2.0.7

--- mk_dist.sh
+++ mk_dist.sh
@@ -145,6 +145,7 @@ cat external/popper.min.js \
     external/annyang.min.js \
     external/speechkitt/speechkitt.min.js \
     external/dropify/dropify.min.js \
+    external/masonry.pkgd.min.js \
     external/fontawesome/brands.min.js \
     external/fontawesome/solid.min.js > ws_dist/min.external.js
 
