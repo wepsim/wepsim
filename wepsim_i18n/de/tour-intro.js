70ff0227f65df9f9c44ccd65807fc637496d057b
Alejandro <acaldero@inf.uc3m.es>
2019-09-21 13:19:48 +0200

    2.0.B: minor improvements

--- wepsim_i18n/de/tour-intro.js
+++ wepsim_i18n/de/tour-intro.js
@@ -22,11 +22,24 @@
     i18n.eltos.tour_intro.de = {
 
 		'step1':		'WepSIM hilft dabei, die Funktionsweise eines Computers besser zu verstehen: Es ist visuell, interaktiv, integriert von Signalen bis hin zu Unterbrechungen, Systemaufrufen, Ausnahmen usw. <br> <br> Wir sind der festen Überzeugung, dass WepSIM ein revolutionäres Lehrmittel ist. Diese kurze Tour führt in die wichtigsten Elemente der Benutzeroberfläche ein.',
-		'step2':		'Über diese Schaltfläche oben rechts können Benutzer den Ausführungsmodus auswählen. <br> <br> Zum Beispiel die Hardware, mit der gearbeitet werden soll (z. B. der Elementarprozessor oder die EP). <br> Oder den Lernmodus, der in der empfohlen wird Anfang ;-).',
+
+		'step2': 	   "Diese Schaltfläche oben rechts ist ein Schnellzugriffsmenü auf verschiedene 'Arbeitsmodi'. <br>" +
+                                   "<br>" +
+                                   "Benutzer können auswählen:" +
+                                   "<ul>" +
+                                   "<li>Die Hardware, mit der gearbeitet werden soll (z. B. EP - Prozessor usw.)</li>" +
+                                   "<li>Nur Assembly-Modus mit ganzzahligen MIPS <sub> 32 </ sub> - oder RISC-V <sub>32</sub>-Anweisungen</li>" +
+                                   "<li>Der zu Beginn empfohlene Lernmodus; -)</li>" +
+                                   "</ul>",
+
 		'step3':		'Oben rechts öffnet die Schaltfläche "Hilfe" den zugehörigen Dialog. <br> <br> Der Hilfedialog fasst die Tutorials, Beschreibungen, Informationen usw. zusammen.',
+
 		'step4':		'Und auf der linken Seite öffnet die Schaltfläche "Beispiele" den Beispieldialog. <br> <br> Es gibt viele Beispiele, die zum schrittweisen Lernen verwendet werden können.',
+
 		'step5':		'Oben links wird über die Schaltfläche "Konfiguration" der Konfigurationsdialog geöffnet. <br> <br> Mit dieser Schaltfläche können Benutzer verschiedene Aspekte der Ausführung, der Benutzeroberfläche, der Einstellungen usw. anpassen.',
+
 		'step6':		'Herzlichen Glückwunsch! Sie kennen die wichtigsten Elemente der WepSIM-Benutzeroberfläche. <br> Über das Dialogfeld "Hilfe" können Sie auf das Lernprogramm "Willkommen" zugreifen, um mit dem Lernen fortzufahren. <br>',
+
 		'_last_':		'_last_'
 
     };
--- wepsim_i18n/pt/tour-intro.js
+++ wepsim_i18n/pt/tour-intro.js
@@ -22,7 +22,14 @@
     i18n.eltos.tour_intro.pt = {
 
 		'step1':		'O WepSIM ajuda a entender melhor como funciona um computador: é visual, interativo, integra desde sinais até interrupções, chamadas de sistema, exceções, etc. <br> <br> Nós realmente acreditamos que o WepSIM é uma ferramenta de ensino revolucionária. Este breve tour apresenta os principais elementos de sua interface.',
-		'step2':		'Este botão no canto superior direito permite que os usuários escolham o \'modo de execução\'. <br> <br> Por exemplo, o hardware para trabalhar (por exemplo, o processador elementar ou EP). <br> Ou o modo tutorial, recomendado no começando ;-).',
+	        "step2":                "Este botão no canto superior direito é um menu de acesso rápido aos diferentes 'modos de trabalho'. <br>" +
+                                        "<br>" +
+                                        "Os usuários podem selecionar:" +
+                                        "<ul>" +
+                                        "<li>O hardware para trabalhar (por exemplo, processador EP, etc.)</li>" +
+                                        "<li>Modo somente montagem, com instruções MIPS<sub>32</sub> inteiras ou RISC-V<sub>32</sub></li>" +
+                                        "<li>O modo tutorial, recomendado no início ;-)</li>" +
+                                        "</ul>",
 		'step3':		'No canto superior direito, o botão \'ajuda\' abre a caixa de diálogo associada. <br> <br> A caixa de diálogo de ajuda resume os tutoriais, descrições, informações etc.',
 		'step4':		'E à esquerda, o botão \'exemplos\' abre o diálogo de exemplo. <br> <br> Há muitos exemplos que podem ser usados ​​para aprender de forma incremental.',
 		'step5':		'No canto superior esquerdo, o botão \'configuração\' abre o diálogo de configuração. <br> <br> Permite que os usuários adaptem vários aspectos da execução, interface do usuário, preferências, etc.',
