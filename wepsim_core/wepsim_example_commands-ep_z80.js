c25958681e7f9551d8b9c41cde488bc343ff4bae
Alejandro Calderon Mateos <acaldero.uc3m@gmail.com>
2019-10-12 05:24:39 +0200

    2.0.B: z80-like initial support

---
+++ wepsim_core/wepsim_example_commands-ep_z80.js
@@ -0,0 +1,35 @@
+/*
+ *  Copyright 2015-2019 Felix Garcia Carballeira, Alejandro Calderon Mateos, Javier Prieto Cepeda, Saul Alonso Monsalve
+ *
+ *  This file is part of WepSIM.
+ *
+ *  WepSIM is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  WepSIM is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with WepSIM.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+    // EP+Z80
+
+    ws_examples.push({
+                       id: 'S8E1',
+                       title: "<span data-langkey='Instructions'>Instructions</span>",
+                       type: "<span data-langkey='Initial'>Initial</span>",
+                       level: "actual",
+                       modes:  "newbie,intro,asm_z80",
+                       hardware:  "ep",
+                       microcode: "ep_z80",
+                       assembly:  "ep_s8_e1",
+                       description: "<span data-langkey='example_01_01'>Simple example with fetch, arithmetic instructions, and basic .text segment.</span>"
+                     });
+
