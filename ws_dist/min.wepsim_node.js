bad4686d6237908da67ca1cb307d7d4f30b22948
Alejandro <acaldero@inf.uc3m.es>
2019-10-26 19:45:42 +0200

    2.0.11: back-porting show-* for wepsim-node

--- ws_dist/min.wepsim_node.js
+++ ws_dist/min.wepsim_node.js
@@ -192,6 +192,21 @@ var sim={systems:[],active:null,index:0};function simhw_add(a){sim.systems.push(
     {
         var ret = null ;
  
+	// 0) components: not run needed
+        if ("SHOW-RECORD"    == data.action) {
+            ret = wepsim_nodejs_show_record(data.record) ;
+            console.log(ret) ;
+            return true ;
+	}
+        if ("SHOW-MICROCODE" == data.action) {
+            console.log(data.firmware) ;
+            return true ;
+	}
+        if ("SHOW-ASSEMBLY"  == data.action) {
+            console.log(data.assembly) ;
+            return true ;
+	}
+
 	// 1) initialization
         wepsim_nodejs_init(data.mode) ;
 
@@ -206,9 +221,6 @@ var sim={systems:[],active:null,index:0};function simhw_add(a){sim.systems.push(
 	// 3) return result
         if ("SHOW-CONSOLE" == data.action) {
             ret.msg = get_screen_content() ;
-	}
-        if ("SHOW-RECORD" == data.action) {
-            ret.msg = wepsim_nodejs_show_record(data.record) ;
 	}
         if ("RUN" == data.action) {
             ret = wepsim_nodejs_show_currentstate() ;
@@ -371,7 +383,9 @@ var sim={systems:[],active:null,index:0};function simhw_add(a){sim.systems.push(
                 ' * ./wepsim_node.sh <command> <hardware name> <microcode file> <assembly file> [<checklist file>] [options*]\n' +
                 ' * ./wepsim_node.sh <command> checkpoint      <checkpoint file>                [<checklist file>] [options*]\n' +
                 '\n' +
-                '    <command>         = run | stepbystep | microstepbymicrostep | check | microstepverbalized | show-console | show-record\n' +
+                '    <command>         = run | stepbystep | microstepbymicrostep | check |\n' +
+                '                        show-console | microstepverbalized |\n' +
+                '                        show-record | show-microcode | show-assembly | build-checkpoint\n' +
                 '    <hardware name>   = ep | poc\n' +
                 '\n' +
                 '    <checkpoint file> = "path to the checkpoint file" \n' +
@@ -440,10 +454,13 @@ var sim={systems:[],active:null,index:0};function simhw_add(a){sim.systems.push(
                 ' * Show recorded session:\n' +
                 '   ./wepsim_node.sh show-record checkpoint ./examples/checkpoint/tutorial_1.txt\n' +
                 '\n' +
+                ' * Build checkpoint from assembly and microcode, and print it to standard output:\n' +
+                '   ./wepsim_node.sh build-checkpoint ep ./examples/microcode/mc-ep_base.txt ./examples/assembly/asm-ep_s1_e1.txt\n' +
+                '\n' +
                 ' * Export hardware definition as JSON:\n' +
                 '   ./wepsim_node.sh export-hardware ep > examples/hardware/ep/hw_def.json\n' +
                 '\n' +
-                ' * Build MIPS32-like microcode for testing:\n' +
+                ' * Build MIPS32-like microcode for testing in command-line:\n' +
                 '   ./wepsim_node.sh import-creator checkpoint ./MIPS-32-like.json > microcode.txt\n' +
                 '   ./wepsim_node.sh run ep ./microcode.txt examples/assembly/asm-ep_s6_e3.txt\n' +
                 '' ;
@@ -533,6 +550,18 @@ var sim={systems:[],active:null,index:0};function simhw_add(a){sim.systems.push(
     hash_action["SHOW-RECORD"] = wepsim_nodejs_runApp ;
  
     //
+    // SHOW-MICROCODE
+    //
+ 
+    hash_action["SHOW-MICROCODE"] = wepsim_nodejs_runApp ;
+ 
+    //
+    // SHOW-ASSEMBLY
+    //
+ 
+    hash_action["SHOW-ASSEMBLY"] = wepsim_nodejs_runApp ;
+ 
+    //
     // SHOW-CONSOLE
     //
  
@@ -564,6 +593,36 @@ var sim={systems:[],active:null,index:0};function simhw_add(a){sim.systems.push(
         return true ;
     } ;
 
+    //
+    // BUILD-CHECKPOINT
+    //
+ 
+    hash_action["BUILD-CHECKPOINT"] = function(data, options)
+    {
+        // pack elements
+        var checkpointObj = {
+                              "mode":          data.mode,
+                              "firmware":      data.firmware,
+                              "assembly":      data.assembly,
+                              "state_current": {
+                                                  time:        Date().toString(),
+                                                  title:       '',
+                                                  title_short: '',
+				                  content:     ''
+				               },
+                              "state_history": [],
+                              "record":        '',
+                              "tag":           Date().toString(),
+                              "notify":        true
+                           } ;
+
+        var checkpointNB  = wepsim_checkpoint_Obj2NB(checkpointObj) ;
+        var checkpointStr = JSON.stringify(checkpointNB, null, 2) ;
+        console.log(checkpointStr);
+
+        // return ok
+        return true ;
+    } ;
 
     /**
      * WepSIM actions
