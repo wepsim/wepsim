6e1fa1ef8ddf76d121f28f3b0e2b54d16c560824
Alejandro Calderon Mateos <acaldero.uc3m@gmail.com>
2022-11-09 03:44:29 +0100

    2.2.1: minor updates

--- sim_core/sim_adt_cachememory.js
+++ sim_core/sim_adt_cachememory.js
@@ -29,14 +29,13 @@
          *               "cfg":   {
          *                           tag_size: 1,
          *                           set_size: 1,
-         *                           off_size: 1
+         *                           off_size: 1,
+         *                           replace_pol: "first"
          *                        },
          *               "sets":  {
          *                           0: {
          *                                 tags: {
-         *                                          0: {
-         *                                                n_access:0
-         *                                             }
+         *                                          0: { n_access:0, valid:0, dirty:0 }
          *                                       }
          *                              }
          *                        }
@@ -44,35 +43,10 @@
          *            }
          */
 
-        function cache_memory_init ( )
-        {
-            return {
-                       "stats": {
-                                   n_access: 0,
-                                   n_hits:   0,
-                                   n_misses: 0
-                                },
-                       "cfg":   {
-                                   tag_size: 1,
-                                   set_size: 1,
-                                   off_size: 1
-                                },
-                       "sets":  { }
-                   } ;
-        }
 
-        function cache_memory_update_cfg ( memory_cfg, tag_size, set_size, off_size )
-        {
-            memory_cfg.tag_size    = tag_size ;
-            memory_cfg.set_size    = set_size ;
-            memory_cfg.offset_size = off_size ;
-
-            memory_cfg.mask_tag = (Math.pow(2, tag_size) - 1) ;
-            memory_cfg.mask_tag = (memory_cfg.mask_tag << (32 - tag_size)) >>> 0 ;
-            memory_cfg.mask_set = (Math.pow(2, set_size) - 1) ;
-            memory_cfg.mask_set = (memory_cfg.mask_set << (off_size)) >>> 0 ;
-            memory_cfg.mask_off = (Math.pow(2, off_size) - 1) >>> 0 ;
-        }
+        //
+        // Auxiliar
+        //
 
         function cache_memory_split ( memory_cfg, address )
         {
@@ -89,7 +63,89 @@
             return parts ;
         }
 
-        function cache_memory_access ( memory, address )
+        function cache_memory_stats_updatehit ( memory, set, tag, r_w )
+        {
+            // global stats
+            memory.stats.n_access++ ;
+            memory.stats.n_hits++ ;
+
+            // block stats
+            (memory.sets[set].tags[tag].n_access)++ ;
+            if (r_w == "write") {
+                memory.sets[set].tags[tag].dirty = 1 ;
+            }
+        }
+
+        function cache_memory_stats_updatemiss ( memory, set, tag, r_w )
+        {
+            // global stats
+            memory.stats.n_access++ ;
+            memory.stats.n_misses++ ;
+
+            // block stats
+            (memory.sets[set].tags[tag].n_access)++ ;
+            if (r_w == "write") {
+                memory.sets[set].tags[tag].dirty = 1 ;
+            }
+        }
+
+        function cache_memory_select_victim ( memory, set )
+        {
+            var keys = Object.keys(memory.sets[set].tags) ;
+            var tag_victim  = 0 ;
+            var tag_naccess = 0 ;
+
+            if (memory.cfg.replace_pol == "lfu")
+            {
+		tag_victim  = keys[0] ;
+                tag_naccess = memory.sets[parts.set].tags[tag_victim].n_access ;
+		for (var i=1; i<keys.length; i++) {
+                     if (tag_naccess > memory.sets[parts.set].tags[keys[i]].n_access) {
+		         tag_victim = keys[i] ;
+                         tag_naccess = memory.sets[parts.set].tags[tag_victim].n_access ;
+                     }
+                }
+            }
+
+            if (memory.cfg.replace_pol == "first") {
+                tag_victim = keys[0] ;
+            }
+
+            return tag_victim ;
+        }
+
+
+        //
+        // API
+        //
+
+        function cache_memory_init ( tag_size, set_size, off_size )
+        {
+            var c = {
+                       "stats": {
+                                   n_access: 0,
+                                   n_hits:   0,
+                                   n_misses: 0
+                                },
+                       "cfg":   {
+                                   tag_size: tag_size,
+                                   set_size: set_size,
+                                   off_size: off_size,
+                                   replace_pol: "first"
+                                },
+                       "sets":  { }
+                   } ;
+
+            c.cfg.mask_tag = (Math.pow(2, tag_size) - 1) ;
+            c.cfg.mask_tag = (c.cfg.mask_tag << (32 - tag_size)) >>> 0 ;
+            c.cfg.mask_set = (Math.pow(2, set_size) - 1) ;
+            c.cfg.mask_set = (c.cfg.mask_set << (off_size)) >>> 0 ;
+            c.cfg.mask_off = (Math.pow(2, off_size) - 1) >>> 0 ;
+
+            return c ;
+        }
+
+        function cache_memory_access ( memory, address, r_w )
         {
             // divide address into set:tag:offset
             var parts = cache_memory_split(memory.cfg, address) ;
@@ -97,43 +153,55 @@
             // initialize set (if not done before)
             if (typeof memory.sets[parts.set] == "undefined")
             {
-                memory.sets[parts.set] = {
-                                            tags:{},
-                                            number_tags:0
-                                         } ;
+                memory.sets[parts.set] = { tags:{}, number_tags:0 } ;
             }
 
             // if tag is loaded in any entry of the set -> update stats and return hit (true)
-            if (typeof memory.sets[parts.set].tags[parts.tag] != "undefined")
+            if ( (typeof memory.sets[parts.set].tags[parts.tag] != "undefined") &&
+                        (memory.sets[parts.set].tags[parts.tag].valid == 1) )
             {
-                (memory.sets[parts.set].tags[parts.tag].n_access)++ ;
-                memory.stats.n_access++ ;
-                memory.stats.n_hits++ ;
-
+                cache_memory_stats_updatehit(memory, parts.set, parts.tag, r_w) ;
                 return true ;
             }
 
             // tag is not loaded in set... make room first?
             if (typeof memory.sets[parts.set].number_tags > 3)
             {
-                var keys = Object.keys(memory.sets[parts.set].tags) ;
+                var tag_victim = cache_memory_select_victim(memory, parts.set) ;
 
-                // <TODO>
-                // * select key to kick-out... index in keys
-                var i = 0 ;
-                // </TODO>
-
-                delete memory.sets[parts.set].tags[keys[i]] ;
+                memory.sets[parts.set].tags[tag_victim].valid = 0 ;
                 memory.sets[parts.set].number_tags-- ;
             }
 
             // load tag in set, update stats and return miss (false)
-            memory.sets[parts.set].tags[parts.tag] = { n_access:1 } ;
+            memory.sets[parts.set].tags[parts.tag] = { n_access:0, valid:1, dirty:0 } ;
             memory.sets[parts.set].number_tags++ ;
 
-            memory.stats.n_access++ ;
-            memory.stats.n_misses++ ;
-
+            cache_memory_stats_updatemiss(memory, parts.set, parts.tag, r_w) ;
             return false ;
+        }
+
+        function cache_memory_stats_show ( memory )
+        {
+            var o = "" ;
+
+            o += "" +
+                 "cfg\n" +
+                 "---\n" +
+                 "* tag_size:    " + memory.cfg.tag_size + "\n" +
+                 "* set_size:    " + memory.cfg.set_size + "\n" +
+                 "* offset_size: " + memory.cfg.off_size + "\n" +
+                 "* replace_pol: " + memory.cfg.replace_pol + "\n" +
+                 "\n" +
+                 "stats\n" +
+                 "-----\n" +
+                 "* access: " + memory.stats.n_access + "\n" +
+                 "* hits:   " + memory.stats.n_hits   + "\n" +
+                 "* misses: " + memory.stats.n_misses + "\n" +
+                 "* hit  ratio: " + (memory.stats.n_hits   / memory.stats.n_access) + "\n" +
+                 "* miss ratio: " + (memory.stats.n_misses / memory.stats.n_access) + "\n" +
+                 "\n" ;
+
+            return o ;
         }
 
