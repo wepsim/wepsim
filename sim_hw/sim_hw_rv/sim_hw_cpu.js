e36c3779ea83f3275374946800813e9369127ec5
Alejandro Calderon Mateos <acaldero.uc3m@gmail.com>
2023-05-20 04:33:53 +0200

    2.3.0-rv: minor updates

--- sim_hw/sim_hw_rv/sim_hw_cpu.js
+++ sim_hw/sim_hw_rv/sim_hw_cpu.js
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2015-2023 Felix Garcia Carballeira, Alejandro Calderon Mateos, Javier Prieto Cepeda, Saul Alonso Monsalve
+ *  Copyright 2015-2023 Felix Garcia Carballeira, Alejandro Calderon Mateos, Javier Prieto Cepeda, Saul Alonso Monsalve, Juan Banga Pardo
  *
  *  This file is part of WepSIM.
  *
