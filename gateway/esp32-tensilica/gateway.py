bc538fbc7121420b88da4170385cefb95468ce3a
Alejandro Calderon Mateos <acaldero.uc3m@gmail.com>
2023-06-05 21:56:33 +0200

    2.3.0: minor updates

--- gateway/esp32-tensilica/gateway.py
+++ gateway/esp32-tensilica/gateway.py
@@ -4,20 +4,20 @@
 #
 #  Copyright 2022-2023 Felix Garcia Carballeira, Diego Carmarmas Alonso, Alejandro Calderon Mateos
 #
-#  This file is part of CREATOR.
+#  This file is part of WepSIM.
 #
-#  CREATOR is free software: you can redistribute it and/or modify
+#  WepSIM is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU Lesser General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
 #
-#  CREATOR is distributed in the hope that it will be useful,
+#  WepSIM is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU Lesser General Public License for more details.
 #
 #  You should have received a copy of the GNU Lesser General Public License
-#  along with CREATOR.  If not, see <http://www.gnu.org/licenses/>.
+#  along with WepSIM.  If not, see <http://www.gnu.org/licenses/>.
 #
 
 
