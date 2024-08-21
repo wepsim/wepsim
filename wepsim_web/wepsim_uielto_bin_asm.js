059cbd892646220e57eea83b1cc5ffd2c30d3127
Alejandro Calderon Mateos <acaldero.uc3m@gmail.com>
2024-08-21 19:00:55 +0200

    2.3.5: minor updates

--- wepsim_web/wepsim_uielto_bin_asm.js
+++ wepsim_web/wepsim_uielto_bin_asm.js
@@ -140,11 +140,12 @@
                                         'c_end':   parseInt(seg[skey1].end),
                                         'm_begin': parseInt(seg[skey1].begin),
                                         'm_end':   0,
+                                        'loaded':  seg[skey1].loaded,
 		                        'color':   seg[skey1].color
 				      } ;
 
                      // try to use the limits loaded in main memory (if any) ...
-                     if (seg[skey1].loaded)
+                     if (slimits[skey1].loaded)
                           slimits[skey1].m_end = slimits[skey1].c_end ;
                      else slimits[skey1].m_end = slimits[skey1].c_begin + WORD_BYTES ;
                 }
@@ -175,12 +176,24 @@
 			 "<th class='border border-0' align='right'>&nbsp;&nbsp;segment</th>" +
 			 "</tr>" ;
 
-	   	var color="white";
+	   	var color = "white" ;
+	   	var sname = "" ;
 	        for (var skey in seg)
 	        {
+                     // tip: ".binary" is a segment section but not a memory segment,
+                     //      ".stack"  is a memory segment but not a segment section
+	   	     sname = seg[skey].name ;
+                     if (".binary" == skey)
+                     {
+                          if (false == slimits[skey].loaded)
+	   	               continue ;   // skip empty ".binary" segment section
+                          else sname = "" ; // skip ".binary" segment as memory segment name
+                     }
+
                      c_begin =  slimits[skey].m_begin ;
                      c_end   =  slimits[skey].m_end ;
 		     color   =  slimits[skey].color ;
+
                      rows    =  0 ;
                      var x   =  "" ;
                      var p   =  "" ;
@@ -221,7 +234,7 @@
                          rows = 2 ;
 		     }
 
-                     o += rows + " class=\"text-dark\" bgcolor=\"" + color + "\" align=right>" + seg[skey].name + "&nbsp;</td></tr>" + x ;
+                     o += rows + " class=\"text-dark\" bgcolor=\"" + color + "\" align=right>" + sname + "&nbsp;</td></tr>" + x ;
 
 	             if (seg[skey].name != ".stack") {
 		         o += "<tr class=\"font-monospace fs-6 text-dark\">" +
