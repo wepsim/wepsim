30be32e5430fd1e871f79f2334f8712baf0d5426
Alejandro Calderon Mateos <acaldero.uc3m@gmail.com>
2024-12-21 14:12:08 +0100

    2.3.6: initial release

--- sim_core/sim_cfg.js
+++ sim_core/sim_cfg.js
@@ -216,8 +216,8 @@
         {
              var wscfg = {
                    /* version */
-                   "version":               { upgrade:false, type:"string",    value:"2.3.5" },
-                   "build":                 { upgrade:true,  type:"string",    value:"2.3.5.20240902A" },
+                   "version":               { upgrade:false, type:"string",    value:"2.3.6" },
+                   "build":                 { upgrade:true,  type:"string",    value:"2.3.6.20241212A" },
 
 	           /* simulation screen: SVG */
                    "color_data_active":     { upgrade:false, type:"string",    value:"#0066FF" },
