ec59a15e18635f2057a4a8f4554d03f8d7bbaae9
Alejandro Calderon Mateos <acaldero.uc3m@gmail.com>
2026-05-10 21:40:57 +0200

    2.3.9: minor updates

--- sim_sw/assembly/memory_segments.js
+++ sim_sw/assembly/memory_segments.js
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2015-2026 Saul Alonso Monsalve, Javier Prieto Cepeda, Felix Garcia Carballeira, Alejandro Calderon Mateos
+ *  Copyright 2015-2026 The WepSIM team (see docs/WEPSIM-TEAM.md)
  *
  *  This file is part of WepSIM.
  *
